d9e735bb063ec0623af119906774ef7a180ba7b9
Peter Topping <peter@bymiles.co.uk>
2018-09-07 21:32:09 +0100

    setup for next and next-sass

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import '../static/styles/index.scss';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <h1>html has displayed!!</h1>
+        );
+    }
+}
+
+export default Dashboard;
